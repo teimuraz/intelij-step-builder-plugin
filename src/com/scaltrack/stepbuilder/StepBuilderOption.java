91487b5c9a21bc1cfe359e1b34e99fe973a6d02b
teimuraz <teimuraz.kantaria@gmail.com>
2019-03-03 02:10:29 +0400

    Initial commit.

---
+++ src/com/scaltrack/stepbuilder/StepBuilderOption.java
@@ -0,0 +1,19 @@
+package com.scaltrack.stepbuilder;
+
+public enum StepBuilderOption {
+
+    FINAL_SETTERS("finalSetters"),
+    COPY_CONSTRUCTOR("copyConstructor"),
+    WITH_JAVADOC("withJavadoc"),
+    PUBLIC_INTERFACES("publicInterface");
+
+    private final String property;
+
+    private StepBuilderOption(final String property) {
+        this.property = String.format("GenerateStepBuilder.%s", property);
+    }
+
+    public String getProperty() {
+        return property;
+    }
+}
